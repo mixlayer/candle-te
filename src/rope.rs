027f8e9dc3bf512bb7995d3e59788c3ec75965cb
Zack Angelo <zackangelo@gmail.com>
2025-06-21 23:33:10 +0000

    wip

--- src/rope.rs
+++ src/rope.rs
@@ -1,5 +1,3 @@
-use std::ops::Deref;
-
 use crate::sys;
 
 use candle::{
@@ -7,25 +5,9 @@ use candle::{
     backend::BackendStorage,
     cuda::{
         CudaDType, WrapErr as _,
-        cudarc::{
-            self,
-            driver::{CudaSlice, CudaStream, DevicePtr, DevicePtrMut, DeviceRepr},
-        },
+        cudarc::driver::{CudaSlice, CudaStream, DevicePtr, DeviceRepr},
     },
 };
-
-fn tensor_device_ptr<T: CudaDType>(t: &Tensor, stream: &cudarc::driver::CudaStream) -> Result<u64> {
-    let (s, l) = t.storage_and_layout();
-    match &*s {
-        candle::Storage::Cuda(cuda_storage) => {
-            //TODO this _sync is almost certainly being dropped too early either causing
-            // unsafety or perf degradation
-            let (ptr, _sync) = cuda_storage.as_cuda_slice::<T>()?.device_ptr(&stream);
-            Ok(ptr)
-        }
-        _ => panic!("nvte only supports cuda"),
-    }
-}
 
 pub fn nvte_fused_rope(
     input: &Tensor,
@@ -34,6 +16,12 @@ pub fn nvte_fused_rope(
     start_positions: &Tensor, //[i32]
     interleaved: bool,
 ) -> Result<Tensor> {
+    println!("fused rope input = {:?}", input);
+    println!("cu_seqlens = {}", cu_seqlens);
+    println!("freqs = {:?}", freqs);
+    println!("start_positions = {}", start_positions);
+    println!("interleaved = {:?}", interleaved);
+
     let rope = NVTEFusedRope::new(
         cu_seqlens.clone(),
         freqs.clone(),
@@ -60,10 +48,19 @@ pub struct NVTETensor {
 }
 
 impl NVTETensor {
-    pub fn from_tensor<T: CudaDType + DeviceRepr>(tensor: &Tensor) -> Result<Self> {
+    pub fn from_tensor(tensor: &Tensor) -> Result<Self> {
         let (storage, layout) = tensor.storage_and_layout();
+
         match &*storage {
-            Storage::Cuda(cuda_storage) => Self::from_storage::<T>(cuda_storage, &layout),
+            Storage::Cuda(cuda_storage) => match tensor.dtype() {
+                DType::U8 => Self::from_storage::<u8>(cuda_storage, &layout),
+                DType::U32 => Self::from_storage::<u32>(cuda_storage, &layout),
+                DType::I64 => Self::from_storage::<i64>(cuda_storage, &layout),
+                DType::BF16 => Self::from_storage::<half::bf16>(cuda_storage, &layout),
+                DType::F16 => Self::from_storage::<half::f16>(cuda_storage, &layout),
+                DType::F32 => Self::from_storage::<f32>(cuda_storage, &layout),
+                DType::F64 => Self::from_storage::<f64>(cuda_storage, &layout),
+            },
             _ => panic!("nvte only supports cuda"),
         }
     }
@@ -147,14 +144,9 @@ impl NVTEFusedRope {
         layout: &Layout,
     ) -> Result<(CudaStorage, Shape)> {
         let stream = input.device().cuda_stream();
-        // let cu_seqlens_ptr = tensor_device_ptr::<u32>(&self.cu_seqlens, &stream)?;
-        // let freqs_ptr = tensor_device_ptr::<f32>(&self.freqs, &stream)?;
-        // let start_positions_ptr = tensor_device_ptr::<u32>(&self.start_positions, &stream)?;
-
-        let cu_seqlens_nvte_tensor = NVTETensor::from_tensor::<u32>(&self.cu_seqlens)?;
-        let start_positions_nvte_tensor = NVTETensor::from_tensor::<u32>(&self.start_positions)?;
-        let freqs_nvte_tensor = NVTETensor::from_tensor::<f32>(&self.freqs)?;
-
+        let cu_seqlens_nvte_tensor = NVTETensor::from_tensor(&self.cu_seqlens)?;
+        let start_positions_nvte_tensor = NVTETensor::from_tensor(&self.start_positions)?;
+        let freqs_nvte_tensor = NVTETensor::from_tensor(&self.freqs)?;
         let input_nvte_tensor = NVTETensor::from_storage::<T>(input, layout)?;
         let output = unsafe { stream.alloc::<T>(layout.shape().elem_count()).w()? };
         let output_nvte_tensor =
@@ -162,18 +154,22 @@ impl NVTEFusedRope {
 
         let qkv_format = sys::NVTE_QKV_Format_NVTE_THD;
 
-        let (t, h, d) = layout.shape().dims3()?;
+        let (full_t, h, d) = layout.shape().dims3()?;
+        let b_sz = self.start_positions.shape().dims1()?;
+        let t = full_t / b_sz;
 
         let d2 = if self.interleaved { d } else { d / 2 };
         let cp_size = 1;
         let cp_rank = 0;
 
         let stride_d = 1;
-        let stride_h = h;
+        let stride_h = d;
         let stride_t = h * d;
         let stride_b = 0;
 
-        let b_sz = self.cu_seqlens.shape().dims1()?;
+        dbg!(
+            t, b_sz, h, d, d2, cp_size, cp_rank, stride_t, stride_b, stride_h, stride_d
+        );
 
         unsafe {
             sys::nvte_fused_rope_forward(
@@ -212,6 +208,7 @@ impl CustomOp1 for NVTEFusedRope {
     ) -> Result<(candle::CudaStorage, candle::Shape)> {
         match storage.dtype() {
             candle::DType::BF16 => self.cuda_fwd_t::<half::bf16>(storage, layout),
+            candle::DType::F32 => self.cuda_fwd_t::<f32>(storage, layout),
             _ => panic!("nvte_fused_rope only supports bf16"),
         }
     }
